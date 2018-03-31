9d89a42398bd1308095b5141b31689e3c2588c49
Vlad <vladyslavyantsov@gmail.com>
2018-04-01 00:07:48 +0200

    layout done

---
+++ js/script.js
@@ -0,0 +1,57 @@
+const calculateStrength = function (password) {
+    let strength = 0;
+
+    const regExp = [
+    /[A-Z]/,
+    /[a-z]/,
+    /[0-9]/,
+    /.{8}/,
+    /.{14}/,
+    /[!-//:-@[-`{-ÿ]/
+  ];
+
+    regExp.forEach(function (regexp) {
+        if (regexp.test(password)) {
+            strength++;
+        }
+    });
+
+    return {
+        value: strength,
+        max: regExp.length
+    };
+};
+
+const checkPasswordStregth = function (password) {
+    let progress = document.querySelector('#passwordStrength'),
+        strength = calculateStrength(this.value);
+
+    progress.value = strength.value;
+    progress.max = strength.max;
+
+
+};
+
+const showProgressBar = function(){
+    let progressBar = document.querySelector('progress');
+    
+    progressBar.style.display = 'inline-block';
+    
+   
+}
+
+const hideProgressBar = function(){
+    let progressBar = document.querySelector('progress');
+    
+    progressBar.style.display = 'none';
+}
+
+
+let input = document.querySelector('#passwordField');
+input.addEventListener('keyup', checkPasswordStregth);
+input.addEventListener('click', showProgressBar);
+input.addEventListener('blur', hideProgressBar);
+
+
+
+    
